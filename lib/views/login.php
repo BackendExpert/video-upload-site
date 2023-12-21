f1c1635ae357bbd85bc977dfa1bace1ad036382d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 21:12:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/login.php
+++ lib/views/login.php
@@ -7,8 +7,8 @@
 <div class="container">
     <div class="form-login">
         <div class="row">
-            <div class="col-lg-4"></div>
-            <div class="col-lg-4">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
                 <div class="card">
                     <div class="card-header">
                         <i class="fas fa-user"></i> Login Here
@@ -16,10 +16,15 @@
                     <div class="card-body">
                         <label for="username">Username : </label><br>
                         <input type="text" name="username" id="" class="form-control"><br>
+
+                        <label for="pass">Password : </label><br>
+                        <input type="password" name="password" id="" class="form-control"><br>
+
+                        <input type="submit" value="Login" class="btn btn-success" name="login_user">
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4"></div>
+            <div class="col-lg-3"></div>
         </div>
     </div>
 </div>
