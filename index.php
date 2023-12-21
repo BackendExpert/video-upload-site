859913be1d6d5b5cfc5a265fac3323d3e898b211
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 20:56:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -15,6 +15,23 @@
                                 <input type="text" name="search" id="" placeholder="Search" class="form-control">
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <select name="vid-len" id="" class="form-control">
+                                    <option value="les10">1 min - 10 min</option>
+                                    <option value="10to20">11 min - 20 min</option>
+                                    <option value="20up">20 min+</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-6">
+                                <select name="vid-qualty" id="" class="form-control">
+                                    <option value="360p">360p</option>
+                                    <option value="480p">480p</option>
+                                    <option value="720p">720p</option>
+                                    <option value="1080p">1080p</option>
+                                </select>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
