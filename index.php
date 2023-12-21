88aa4ce63f15250861261ddf156d0c5f1988c437
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 20:23:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -7,23 +7,7 @@
         <hr>
 
         <div class="row">
-            <div class="col-lg-6">
-                <form action="" method="post">
-                    
-                </form>
-            </div>
-            <div class="col-lg-6">
-                <form action="" method="post">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <input type="text" name="search_tag" id="" class="form-control" placeholder="Search">
-                        </div>
-                        <div class="col-lg-6">
-                            <button type="submit" class="btn btn-success" name="search_tag"><i class="fas fa-search"></i> Search</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            
         </div>
 
     </div>
