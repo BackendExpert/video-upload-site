67f67f2fc4cf988ad974577d750eb2820e154270
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 21:02:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ lib/views/login.php
@@ -0,0 +1,13 @@
+<?php 
+    include("function/function.php");
+    include("layouts/header.php");
+?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+
+
+
+</div>
+
+<script src="../../js/script.js"></script>
