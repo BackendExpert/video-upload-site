a35125155073a6c910f3cb9491a6b0d7876ee4e3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 20:22:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -2,5 +2,29 @@
 <link rel="stylesheet" href="css/style.css">
 <?php include("lib/layouts/nav.php"); ?>
 
+    <div class="container">
+        <h2 class="site-title">Welcome to Site</h2>
+        <hr>
+
+        <div class="row">
+            <div class="col-lg-6">
+
+            </div>
+            <div class="col-lg-6">
+                <form action="" method="post">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <input type="text" name="search_tag" id="" class="form-control" placeholder="Search">
+                        </div>
+                        <div class="col-lg-6">
+                            <button type="submit" class="btn btn-success" name="search_tag"><i class="fas fa-search"></i> Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
 
 <script src="js/script.js"></script>
--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/style.css">
 <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="fas fa-photo-video"></i> Video Site</a>
+    <a class="navbar-brand" href="#"><i class="fas fa-video"></i>  Video Site</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -18,7 +18,8 @@
         </li> -->
       </ul>
       <span class="navbar-text">
-        <a href=""><button class="btn btn-primary"><i class="fas fa-user-alt"></i> Login</button></a>
+        <!-- <a href=""><button class="btn btn-primary"><i class="fas fa-user-alt"></i> Login</button></a> -->
+        <a href="" style="text-decoration:none;"><i class="fas fa-user-alt"></i> Login</a>
       </span>
     </div>
   </div>
