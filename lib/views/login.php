54deacbf4afd9de4055eceda0681b148b028c345
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 21:13:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/login.php
+++ lib/views/login.php
@@ -14,13 +14,15 @@
                         <i class="fas fa-user"></i> Login Here
                     </div>
                     <div class="card-body">
-                        <label for="username">Username : </label><br>
-                        <input type="text" name="username" id="" class="form-control"><br>
+                        <form action="" method="post">
+                            <label for="username">Username : </label><br>
+                            <input type="text" name="username" id="" class="form-control"><br>
 
-                        <label for="pass">Password : </label><br>
-                        <input type="password" name="password" id="" class="form-control"><br>
+                            <label for="pass">Password : </label><br>
+                            <input type="password" name="password" id="" class="form-control"><br>
 
-                        <input type="submit" value="Login" class="btn btn-success" name="login_user" style="width:100%;">
+                            <input type="submit" value="Login" class="btn btn-success" name="login_user" style="width:100%;">
+                        </form>
                     </div>
                 </div>
             </div>
