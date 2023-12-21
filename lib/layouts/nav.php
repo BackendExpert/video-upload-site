5785c88690c1f0434c54ae6e767032bb24cb5c93
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 19:54:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,12 +1,22 @@
 <link rel="stylesheet" href="css/style.css">
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <a class="navbar-brand" href="#"><i class="fas fa-photo-video"></i> Video Site</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li> -->
+      </ul>
       <span class="navbar-text">
         <a href=""><button class="btn btn-primary"><i class="fas fa-user-alt"></i> Login</button></a>
       </span>
