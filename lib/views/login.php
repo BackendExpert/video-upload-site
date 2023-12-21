dfeccd655322ccb55fb840099c666cdcb5c661ea
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-21 21:08:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,13 +1,26 @@
 <?php 
-    include("function/function.php");
-    include("layouts/header.php");
+    include("../function/function.php");
+    include("../layouts/header.php");
 ?>
 <link rel="stylesheet" href="../../css/style.css">
 
 <div class="container">
-
-
-
+    <div class="form-login">
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fas fa-user"></i> Login Here
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4"></div>
+        </div>
+    </div>
 </div>
 
 <script src="../../js/script.js"></script>
