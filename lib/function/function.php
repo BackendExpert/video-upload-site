c0b90c7334f7f8e6db67e6937007fc40a66d86bc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-23 07:16:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -2,6 +2,11 @@
     include("config.php");
     session_start();
 
+
+    function reg_user(){
+        $con = Connection();
+    }
+
     function search_videos($video, $vid_len, $vid_qulty){
         $con = Connection();
 
