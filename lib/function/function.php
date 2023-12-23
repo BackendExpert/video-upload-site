6099ddfd2808f2cc9397d8bacaac8361a6a27109
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-23 08:41:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -56,33 +56,31 @@
         $check_login_user_nor = mysqli_num_rows($check_login_user_result);
         $check_login_user_row = mysqli_fetch_assoc($check_login_user_result);
         
-
-
-        // if($check_login_user_row['is_un_access'] == 1){
-        //     echo "deactive User";
-        // }
-        // else{
-        //     if($check_login_user_row['is_active'] == 1){
-        //         if($pass == $check_login_user_row['user_pass']){
-        //             if(($check_login_user_row['user_type'] == 'user')){
-        //                 setcookie('login',$check_login_user_row['email'],time()+60*60,'/');
-        //                 $_SESSION['loginSession'] = $check_login_user_row['email'];
-        //                 header("location:../routes/user.php");
-        //             }
-        //             elseif($check_login_user_row['user_type'] == 'admin'){
-        //                 setcookie('login',$check_login_user_row['email'],time()+60*60,'/');
-        //                 $_SESSION['loginSession'] = $check_login_user_row['email'];
-        //                 header("location:../routes/admin.php");
-        //             }
-        //         }
-        //         else{
-        //             return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>Password is Doesn't Match...!</div>&nbsp</center>"; 
-        //         }
-        //     }
-        //     else{
-        //         return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>No recodes found..!</div>&nbsp</center>"; 
-        //     }
-        // }
+        if($check_login_user_row['is_un_access'] == 1){
+            echo "deactive User";
+        }
+        else{
+            if($check_login_user_row['is_active'] == 1){
+                if($pass == $check_login_user_row['user_pass']){
+                    if(($check_login_user_row['user_type'] == 'user')){
+                        setcookie('login',$check_login_user_row['email'],time()+60*60,'/');
+                        $_SESSION['loginSession'] = $check_login_user_row['email'];
+                        header("location:../routes/user.php");
+                    }
+                    elseif($check_login_user_row['user_type'] == 'admin'){
+                        setcookie('login',$check_login_user_row['email'],time()+60*60,'/');
+                        $_SESSION['loginSession'] = $check_login_user_row['email'];
+                        header("location:../routes/admin.php");
+                    }
+                }
+                else{
+                    return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>Password is Doesn't Match...!</div>&nbsp</center>"; 
+                }
+            }
+            else{
+                return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>No recodes found..!</div>&nbsp</center>"; 
+            }
+        }
     }
 
     function search_videos($video, $vid_len, $vid_qulty){
