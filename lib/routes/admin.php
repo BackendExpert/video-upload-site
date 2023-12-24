a4a040b801c339cd9c8d198abac823b6639b69fe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-24 19:45:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -170,18 +170,16 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Admin Dashboard</h1>
+        <h5>Date : 
+
+        <?php 
+          $currentDate = date('l, F j, Y');
+          // $currentDate = date("Y-m-d");
+          echo $currentDate;
+        ?>     
+
+      </h5>
       </div>
-
-      <h3>Date : 
-
-      <?php 
-        $currentDate = date('l, F j, Y');
-        // $currentDate = date("Y-m-d");
-        echo $currentDate;
-      ?>     
-
-      </h3>
-      <hr>
 
 
       <div class="row">
--- lib/routes/user.php
+++ lib/routes/user.php
@@ -168,20 +168,37 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
+        <h1 class="h2">Admin Dashboard</h1>
+        <h5>Date : 
+
+        <?php 
+          $currentDate = date('l, F j, Y');
+          // $currentDate = date("Y-m-d");
+          echo $currentDate;
+        ?>     
+
+      </h5>
       </div>
 
-      <h3>Date : 
 
-      <?php 
-        $currentDate = date('l, F j, Y');
-        // $currentDate = date("Y-m-d");
-        echo $currentDate;
-      ?>
-
-      </h3>
-      <hr>
-
+      <div class="row">
+        <div class="col-lg-6">
+          <h3>Personal Info</h3>
+          <div class="card">
+            <div class="bio">
+            <?php update_bio(); ?>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <h3>Channel Info</h3>
+          <div class="card">
+            <div class="bio">
+            <?php update_channel_info(); ?>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
     </main>
