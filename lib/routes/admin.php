36cab914333081cd55b5b501d40792f7383fafc8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-24 19:15:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -138,7 +138,7 @@
             <li class="nav-item">
               <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
                 <i class="fas fa-tachometer-alt"></i>
-                Dashboard
+                Admin Dashboard
               </a>
             </li>
 
