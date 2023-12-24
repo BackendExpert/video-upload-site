c6fe618fd92097fcb4e8534b5c3cab3011a727ed
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-24 08:09:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/video/create.php
+++ lib/routes/video/create.php
@@ -1,11 +1,10 @@
-<?php include("../function/function.php"); ?>
+<?php include("../../layouts/header.php") ?>
+<?php include("../../function/function.php"); ?>
 
 <?php 
 
     if(empty($_SESSION['loginSession'])){
-        header("location:../views/login.php");
+        header("location:../../views/login.php");
     }
 
 ?>
-
-hi all
