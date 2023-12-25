645122b69306858f2d6207a5e7c85d67880efb41
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 09:01:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -367,6 +367,10 @@
             <p><h4>First Name : ". $user_bio_row['fname']. "</h4></p></p>
             <p><h4>Last Name : ". $user_bio_row['lname']. "</h4></p></p>
             <p><h4>Address : ". $user_bio_row['user_address']. "</h4></p></p>
+            <br>
+            <hr>
+
+            <a href=''><button class='jkbtn jkbtn-blue'>Edit Data</button></a>
         ";
 
         echo $user_bio_view;
@@ -376,6 +380,32 @@
         $con = Connection();
 
         $login_email = strval($_SESSION['loginSession']);
+
+        $select_user_bio = "SELECT * FROM user_bio_tbl WHERE email = '$login_email'";
+        $user_bio_result = mysqli_query($con, $select_user_bio);
+        $user_bio_row = mysqli_fetch_assoc($user_bio_result);
+
+        $select_data = "SELECT * FROM user_tbl WHERE email = '$login_email'";
+        $select_result = mysqli_query($con, $select_data);
+        $select_row = mysqli_fetch_assoc($select_result);
+
+        $user_pro_img = 'images/'. $user_bio_row['p_img'];
+
+        $user_bio_view = "
+            <h4>User Profile Image : </h4>
+            <img src='".$user_pro_img."' class='profile-imgs'><br><br>
+
+            <p><h4>Username : ". $select_row['username']. "</h4></p></p>
+            <p><h4>First Name : ". $user_bio_row['fname']. "</h4></p></p>
+            <p><h4>Last Name : ". $user_bio_row['lname']. "</h4></p></p>
+            <p><h4>Address : ". $user_bio_row['user_address']. "</h4></p></p>
+            <br>
+            <hr>
+
+            <a href=''><button class='jkbtn jkbtn-blue'>Edit Data</button></a>
+        ";
+
+        echo $user_bio_view;
     }
 
     function search_videos($video, $vid_len, $vid_qulty){
