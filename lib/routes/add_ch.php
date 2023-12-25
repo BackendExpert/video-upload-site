1d8fce3f3d714d8ff4e51aadf348e8a9e53771aa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 07:58:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/add_ch.php
+++ lib/routes/add_ch.php
@@ -7,3 +7,29 @@
         header("location:../views/login.php");
     }
 ?>
+
+<div class="container">
+    <br><br>
+    <?php go_back(); ?>
+    <hr>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="fname">Channel Profile Image : </label>
+        <input type="file" name="ch_img" id="" class="form-control" accept="image/*"><br>
+
+        <label for="chname">Channel Name : </label>
+        <input type="text" name="ch_name" id="" class="form-control"><br>
+
+        <label for="chennal_desc">Channel Description : </label>
+        <textarea name="ch_desc" cols="30" rows="5" class="form-control" style="resize:none;" placeholder="Channel Description"></textarea><br>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <input type="reset" value="Clear" class="jkbtn jkbtn-gray" style="width:100%;">
+            </div>
+            <div class="col-lg-6">
+                <input type="submit" value="Add Channel Data" class="jkbtn jkbtn-green" name="chdata_add" style="width:100%;">
+            </div>
+        </div>
+    </form>
+</div>
