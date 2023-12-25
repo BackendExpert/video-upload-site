5c4a71acd314ba4d04100f6ef1ae2ba4ad13139b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 06:09:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/add_bio.php
+++ lib/routes/add_bio.php
@@ -1,6 +1,7 @@
 <?php 
     include("../layouts/header.php");
     include("../function/function.php");
+    include("../layouts/nav.php");
 
     if(empty($_SESSION['loginSession'])){
         header("location:../views/login.php");
