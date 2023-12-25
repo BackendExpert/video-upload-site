fd93094641928f65f12a737e5d817b2d568d55a4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 06:22:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/add_bio.php
+++ lib/routes/add_bio.php
@@ -12,4 +12,12 @@
     <br><br>
     <?php go_back(); ?>
     <hr>
+
+    <form action="" method="post">
+        <label for="fname">First Name : </label>
+        <input type="text" name="fn" id="" class="form-control" placeholder="First Name"><br>
+
+        <label for="lname">First Name : </label>
+        <input type="text" name=";n" id="" class="form-control" placeholder="Last Name"><br>
+    </form>
 </div>
