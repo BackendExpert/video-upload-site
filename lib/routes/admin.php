19f60bbcd5e4e9502daf6af962ad3827c830cd79
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 09:16:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -208,7 +208,7 @@
           </div>
         </div>
         <div class="col-lg-3">
-          <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+          <div class="card text-bg-info mb-3" style="max-width: 18rem;">
             <div class="card-header"><i class="fas fa-video"></i> My Likes</div>
             <div class="card-body">
               <h5 class="card-title">All Videos Likes</h5>
