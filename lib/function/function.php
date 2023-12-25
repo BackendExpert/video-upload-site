88694123519e4369964b281aecea45abc3104fde
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 07:15:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -210,16 +210,19 @@
 
     function bio_add_db($pimg, $temp_img, $pimg_size, $fn, $ln, $dob, $address){
         $con = Connection();
-        $target_dir = "images/";
 
-        $img_p = basename($pimg);
-        $target_path = $target_dir . $img_p;
-        $pimg_type = pathinfo($target_path, PATHINFO_EXTENSION);
+        echo $pimg, $temp_img, $pimg_size, $fn, $ln, $dob, $address;
 
-        $allow_images = array('jpg','png','jpeg','gif');
-        if(in_array($pimg_type, $allow_images)){
+        // $target_dir = "images/";
 
-        }
+        // $img_p = basename($pimg);
+        // $target_path = $target_dir . $img_p;
+        // $pimg_type = pathinfo($target_path, PATHINFO_EXTENSION);
+
+        // $allow_images = array('jpg','png','jpeg','gif');
+        // if(in_array($pimg_type, $allow_images)){
+
+        // }
 
     }
 
