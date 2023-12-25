6ac1d51e715cdd60be4d905a360ed5a01d7e2faf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 09:27:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -188,6 +188,48 @@
       </h5>
       </div>
 
+      <h3><i class="fas fa-chart-bar"></i> Channel Stats</h3>
+      <br>
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header"><i class="fas fa-video"></i> My Videos</div>
+            <div class="card-body">
+              <h5 class="card-title">Public Videos</h5>
+              <h4>5</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+            <div class="card-header"><i class="fas fa-video"></i> My Subscribers </div>
+            <div class="card-body">
+              <h5 class="card-title">All Subscribers</h5>
+              <h4>1000</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card text-bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-header"><i class="fas fa-video"></i> My Likes</div>
+            <div class="card-body">
+              <h5 class="card-title">All Videos Likes</h5>
+              <h4>50000</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+            <div class="card-header"><i class="fas fa-video"></i> My Comments</div>
+            <div class="card-body">
+              <h5 class="card-title">All Comments</h5>
+              <h4>12000</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
 
       <div class="row">
         <div class="col-lg-6">
