bdf72548c6464cf0c1694d9b4cb2dc7944158a36
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 09:26:38 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -190,7 +190,7 @@
       
       <a href="admin/add_admin.php"><button class="jkbtn jkbtn-green"><i class="fas fa-user-shield"></i> Add Admin User</button></a>
       <br><hr><br>
-
+      <h3><i class="fas fa-code"></i> Admin Controls</h3>
       <div class="row">
         <div class="col-lg-3">
           <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
@@ -230,7 +230,7 @@
         </div>
       </div>
 
-      <h3><i class="fas fa-code"></i> Admin Controls</h3>
+      
       <hr>
       <br>
       <h3><i class="fas fa-chart-bar"></i> Channel Stats</h3>
