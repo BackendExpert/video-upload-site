7a8919a7f66317800c7a1dca1dae5e2e2ee67655
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-25 07:53:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ lib/routes/add_ch.php
@@ -0,0 +1,9 @@
+<?php 
+    include("../layouts/header.php");
+    include("../function/function.php");
+    include("../layouts/dash_nav.php");
+
+    if(empty($_SESSION['loginSession'])){
+        header("location:../views/login.php");
+    }
+?>
