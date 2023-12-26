23901427e4442e941832d8bcd5e7c8354766c162
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-26 08:11:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

---
+++ lib/routes/admin/loged_his.php
@@ -0,0 +1,21 @@
+<?php include("../../layouts/header.php") ?>
+<?php include("../../function/function.php"); ?>
+<?php include("../../layouts/dash_nav.php") ?>
+
+<link rel="stylesheet" href="../../../css/style.css">
+<?php 
+
+    if(empty($_SESSION['loginSession'])){
+        header("location:../../views/logout.php");
+    }
+    admin_access();
+?>
+
+
+<div class="container">
+    <div class="card">
+        <div class="bio">
+            
+        </div>
+    </div>
+</div>
