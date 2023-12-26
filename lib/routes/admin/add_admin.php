4aff1a9af8abff8f4cd5102fbab4a9b18ebe5137
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-26 07:32:02 +0530

    Updae
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

---
+++ lib/routes/admin/add_admin.php
@@ -0,0 +1,10 @@
+<?php include("../layouts/header.php") ?>
+<?php include("../function/function.php"); ?>
+
+<?php 
+
+    if(empty($_SESSION['loginSession'])){
+        header("location:../views/login.php");
+    }
+    admin_access();
+?>
