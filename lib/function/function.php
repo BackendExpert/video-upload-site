a64abb056c2cf7a53515984755cc03281dbe6243
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-26 07:59:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -421,6 +421,12 @@
             $insert_admin = "INSERT INTO user_tbl(username,email,user_pass,user_type,is_active,is_un_access,channel_status,bio_status,join_at,update_at)VALUES('$username','$email','$pass','admin','1','0','0','0',NOW(),NOW())";
             $insert_result = mysqli_query($con, $insert_admin);
         }
+        else{
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                            <strong>User : </strong> Already exists...!
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>";
+        }
     }
 
     function search_videos($video, $vid_len, $vid_qulty){
