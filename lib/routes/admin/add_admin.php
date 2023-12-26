168afe1264820e83f537cd872f999646b8adaac9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-26 07:49:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/routes/admin/add_admin.php
+++ lib/routes/admin/add_admin.php
@@ -2,6 +2,7 @@
 <?php include("../../function/function.php"); ?>
 <?php include("../../layouts/dash_nav.php") ?>
 
+<link rel="stylesheet" href="../../../css/style.css">
 <?php 
 
     if(empty($_SESSION['loginSession'])){
@@ -16,21 +17,27 @@
     <hr><br>
     <h2><i class="fas fa-plus"></i><i class="fas fa-user-shield"></i> Add Admin</h2><br>
 
-    <form action="" method="post">
-        <label for="Username">Admin Username : </label>
-        <input type="text" name="admin_username" id="" class="form-control" placeholder="Username"><br>
+    <div class="card">
+        <div class="bio">
+            <form action="" method="post">
+                <label for="Username">Admin Username : </label>
+                <input type="text" name="admin_username" id="" class="form-control" placeholder="Username"><br>
 
-        <label for="email">Admin Email : </label>
-        <input type="text" name="admin_email" id="" class="form-control"><br>
+                <label for="email">Admin Email : </label>
+                <input type="text" name="admin_email" id="" class="form-control" placeholder="Email"><br>
 
-        <label for="password">Admin Password : </label>
-        <input type="password" name="admi_pass" id="" class="form-control">
+                <label for="password">Admin Password : </label>
+                <input type="password" name="admi_pass" id="" class="form-control" placeholder="Password"><br>
 
-        <div class="row">
-            <div class="col-lg-6">
-                <input type="reset" value="Clear" class="jkbtn jkbtn-gray">
-            </div>
-            <div class="col-lg-6"></div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <input type="reset" value="Clear" class="jkbtn jkbtn-gray" style="width:100%;">
+                    </div>
+                    <div class="col-lg-6">
+                        <input type="submit" value="Add Admin" class="jkbtn jkbtn-green" name="admin_add" style="width:100%;">
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
