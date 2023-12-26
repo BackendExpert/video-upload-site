ecb3f09e1990ac421f6638c6fbdb08e27e663a01
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-26 07:32:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- lib/routes/admin/add_admin.php
+++ lib/routes/admin/add_admin.php
@@ -1,10 +1,10 @@
-<?php include("../layouts/header.php") ?>
-<?php include("../function/function.php"); ?>
+<?php include("../../layouts/header.php") ?>
+<?php include("../../function/function.php"); ?>
 
 <?php 
 
     if(empty($_SESSION['loginSession'])){
-        header("location:../views/login.php");
+        header("location:../../views/login.php");
     }
     admin_access();
 ?>
