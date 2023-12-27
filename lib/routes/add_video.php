2c83c4235436dcd29239127d50130eb6b1161e80
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-27 20:04:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/routes/add_video.php
+++ lib/routes/add_video.php
@@ -22,10 +22,22 @@
         <input type="text" name="video_tag" id="" class="form-control" placeholder="Video Tag"><br>
 
         <label for="video">Video Description : </label>
-        <textarea name="video_desc" cols="30" rows="5" class="form-control" style="resize:none;" placeholder="Video Description"></textarea>
+        <textarea name="video_desc" cols="30" rows="5" class="form-control" style="resize:none;" placeholder="Video Description"></textarea><br>
 
         <label for="video">Video Public/Private: </label>
-        <input type="file" name="video" id=""><br>
-        
+        <select name="is_public" class="form-control">Video Public/Private : 
+            <option value="1">Public</option>
+            <option value="0">Private</option>
+        </select>
+            
+        <div class="row">
+            <div class="col-lg-6">
+                <input type="reset" value="Clear" class="jkbtm jkbtn-gray" style="width:100%;">
+            </div>
+            <div class="col-lg-6">
+                <input type="submit" value="Add Video" name="video_add" class="jkbtn jkbtn-green" style="width:100%;>
+            </div>
+        </div>
+
     </form>
 </div>
