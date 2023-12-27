2e383787d6a04b9c82eea0b80b974c6577295655
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-27 21:31:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/routes/add_video.php
+++ lib/routes/add_video.php
@@ -34,6 +34,17 @@
         <label for="video_desc">Video Description : </label>
         <textarea name="video_desc" cols="30" rows="5" class="form-control" style="resize:none;" placeholder="Video Description"></textarea><br>
 
+        <label for="video_tag">Video Length (in min) : </label>
+        <input type="text" name="vid_length" id="" class="form-control" placeholder="Video Length"><br>
+
+        <label for="video_type">Video Public/Private: </label>
+        <select name="is_public" class="form-control">Video Public/Private : 
+            <option value="360p">360p</option>
+            <option value="480p">480p</option>
+            <option value="720p">720p</option>
+            <option value="1080p">1080p</option>
+        </select><br>
+
         <label for="video_type">Video Public/Private: </label>
         <select name="is_public" class="form-control">Video Public/Private : 
             <option value="1">Public</option>
