8322da7f9b6ffdb6ff194e881efdb08b95b6ec80
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-27 19:31:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -523,26 +523,7 @@
         $ch_img = 'images/'. $check_row['ch_img'];
 
         $my_channel = "
-            <div class='row'>
-                <div class='col-lg-4'>
-                    <img src='".$ch_img."'>
-                </div>
-                <div class='col-lg-8'>
-                    <div class='row'>
-                        <div class='col-lg-6'>
-                            <div class='card text-bg-primary mb-3' style='max-width: 18rem;'>
-                            <div class='card-header'>Header</div>
-                            <div class='card-body'>
-                                <h5 class='card-title'>Primary card title</h5>
-                                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            </div>
-                        </div>
-                        <div class='col-lg-6'>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         ";
 
         echo $my_channel;
