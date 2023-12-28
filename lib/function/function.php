f46ee34536ecd45e7a5bf6be6425296148a11f39
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-28 08:39:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -614,7 +614,7 @@
 
 
             <div class='col-auto' style='margin-bottom:20px;'>
-                <a href=''>
+                <a href='video_info.php?id=".$video_row['id']."'>
                     <div class='card-body'>
                         <video src='videos/".$video_row['video']."' class='vid-small'></video>
                         <div class='title-video'>
