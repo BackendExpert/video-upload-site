a86833dadb38a36d523863f798f672e4b9f444c2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-01 01:16:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -661,7 +661,7 @@
         }
         else{
             return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-                        <strong>ERROR : </strong> This Type not valied...!
+                        <strong>No Videos Found: </strong> According to your search There are no Videos
                         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                 </div>";
         }
