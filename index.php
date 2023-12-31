6951e2d8f8aaba1bc42f9944186ba016a0fba26f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-01 00:42:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- index.php
+++ index.php
@@ -35,27 +35,6 @@
                                 <input type="text" name="search" id="" placeholder="Search" class="form-control" required>
                             </div>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <label for="lenvid">Video Length</label>
-                                <select name="vid_len" id="" class="form-control">
-                                    <option value="les10">1 min - 10 min</option>
-                                    <option value="10to20">11 min - 20 min</option>
-                                    <option value="20up">20 min+</option>
-                                </select>
-                            </div>
-                            <div class="col-lg-6">
-                                <label for="queltyvid">Video Quality</label>
-                                <select name="vid_qualty" id="" class="form-control">
-                                    <option value="360p">360p</option>
-                                    <option value="480p">480p</option>
-                                    <option value="720p">720p</option>
-                                    <option value="1080p">1080p</option>
-                                </select>
-                            </div>
-                        </div>
-                        <br>
                         <div class="row">
                             <div class="col-lg-12">
                                 <input type="submit" value="Search Videos" name="search_vid" class="jkbtn jkbtn-green">
